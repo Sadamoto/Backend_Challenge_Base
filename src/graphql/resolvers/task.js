9fa8a6eaffe4cdf0ccf59c493ea5c4f66d336c41
Michael Theobald <michael.theobald@abatplus.de>
2020-08-31 14:52:25 +0200

    added summary to format and moved functions out of export

--- src/graphql/resolvers/task.js
+++ src/graphql/resolvers/task.js
@@ -4,6 +4,7 @@ const Task = require('../../models/task');
 const formatTask = task => {
   return {
     _id: task._id.toString(),
+    summary: task.summary.ToString(),
     createdAt: task.createdAt.toISOString(),
     updatedAt: task.updatedAt.toISOString()
   };
@@ -38,60 +39,61 @@ const getTask = async id => {
   return task;
 };
 
-module.exports = {
-  tasks: async ({ excludeCompleted }) => {
-    let rawTasks;
-    if (excludeCompleted) {
-      rawTasks = await Task.find({ isCompleted: true }).sort({ createdAt: -1 });
-    } else {
-      rawTasks = await Task.find().sort({ createdAt: -1 });
-    }
 
-    if (!rawTasks) {
-      const error = new Error('No matching tasks found.');
-      error.code = 404;
-      throw error;
-    }
-
-    const tasks = rawTasks.map(formatTask);
-    return tasks;
-  },
-
-  task: async ({ id }) => {
-    const task = await getTask(id);
-    return formatTask(task);
-  },
-
-  createTask: async ({ summary }) => {
-    validateInput(summary);
-    const task = new Task({ summary });
-    const createdTask = await task.save();
-
-    if (!createdTask) {
-      const error = new Error('Creation of a new task in the database failed.');
-      error.code = 500;
-      throw error;
-    }
-
-    return formatTask(createdTask);
-  },
-
-  updateTask: async ({ id, taskUpdate }) => {
-    const summary = taskUpdate.summary;
-    const isCompleted = taskUpdate.isCompleted;
-    validateInput(summary, isCompleted);
-
-    const task = await getTask(id);
-
-    task.summary = summary;
-    task.isCompleted = isCompleted;
-    const updateTask = task.save();
-    return formatTask(updateTask);
-  },
-
-  deleteTask: async ({ id }) => {
-    await getTask(id);
-    await Task.findByIdAndRemove(id);
-    return true;
+const tasks = async ({ excludeCompleted }) => {
+  let rawTasks;
+  if (excludeCompleted) {
+    rawTasks = await Task.find({ isCompleted: true }).sort({ createdAt: -1 });
+  } else {
+    rawTasks = await Task.find().sort({ createdAt: -1 });
   }
-};
+
+  if (!rawTasks) {
+    const error = new Error('No matching tasks found.');
+    error.code = 404;
+    throw error;
+  }
+
+  const tasks = rawTasks.map(formatTask);
+  return tasks;
+},
+
+const task = async ({ id }) => {
+  const task = await getTask(id);
+  return formatTask(task);
+},
+
+const createTask = async ({ summary }) => {
+  validateInput(summary);
+  const task = new Task({ summary });
+  const createdTask = await task.save();
+
+  if (!createdTask) {
+    const error = new Error('Creation of a new task in the database failed.');
+    error.code = 500;
+    throw error;
+  }
+
+  return formatTask(createdTask);
+},
+
+const updateTask = async ({ id, taskUpdate }) => {
+  const summary = taskUpdate.summary;
+  const isCompleted = taskUpdate.isCompleted;
+  validateInput(summary, isCompleted);
+
+  const task = await getTask(id);
+
+  task.summary = summary;
+  task.isCompleted = isCompleted;
+  const updateTask = task.save();
+  return formatTask(updateTask);
+},
+
+const deleteTask = async ({ id }) => {
+  await getTask(id);
+  await Task.findByIdAndRemove(id);
+  return true;
+}
+
+module.exports = { tasks, task, createTask, updateTask, deleteTask };
